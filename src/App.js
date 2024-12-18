0cef49d799040977b9077e908bef095286940ed3
Prateek Gupta <68748725+prateekgupta2411@users.noreply.github.com>
2024-12-18 18:59:32 +0530

    Course-App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>This is my react JS Application</h1>
+      <p>I am currectly learning ReactJs</p>
+      <p>I am Prateek Gupta</p>
+    </div>
+  );
+}
+
+export default App;
